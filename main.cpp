184a017d255e02b94d5b70652751e81006c7c4d9
matkatmusic <matkatmusic@gmail.com>
2020-05-28 23:51:26 +0000

    replaced main() with one you need to revise to produce the expected output.

--- main.cpp
+++ main.cpp
@@ -27,37 +27,39 @@ struct HeapA
 };
 
 
-/*
- 1) Edit your 3 structs so that they own a heap-allocated primitive type without using smart pointers
- IntType should own a heap-allocated int, for example.
+ /*
+ 1) Edit your 3 structs so that they own a heap-allocated primitive type without using smart pointers named 'value'
+         IntType should own a heap-allocated int, for example.
  
  2) give it a constructor that takes the appropriate primitive
- this argument will initialize the owned primitive's value.
- i.e. if you're owning an int on the heap, your ctor argument will initialize that heap-allocated int's value.
+    this argument will initialize the owned primitive's value.
+         i.e. if you're owning an int on the heap, your ctor argument will initialize that heap-allocated int's value.
  
  3) modify those add/subtract/divide/multiply member functions from chapter 2 on it
- a) make them modify the owned numeric type
- b) set them up so they can be chained together.
- i.e.
- DoubleType dt(3.5);
- dt.add(3.0).multiply(-2.5).divide(7.2); //an example of chaining
+         a) make them modify the owned numeric type
+         b) set them up so they can be chained together.
+             i.e.
+             DoubleType dt(3.5);
+             dt.add(3.0).multiply(-2.5).divide(7.2); //an example of chaining
  
  4) write add/subtract/divide/multiply member functions for each type that take your 3 UDTs
- These are in addition to your member functions that take primitives
- for example, IntType::divide(const DoubleType& dt);
- These functions should return the result of calling the function that takes the primitive.
+        These are in addition to your member functions that take primitives
+        for example, IntType::divide(const DoubleType& dt);
+        These functions should return the result of calling the function that takes the primitive.
+     
  
- 
- 5) print out the results with some creative couts
- i.e.
- FloatType ft(0.1f);
- IntType it(3);
- std::cout << "adding 3 and subtracting 'it' from 'ft' results in the following value: " << *ft.add(2.f).subtract( it ).value << std::endl;  //note the dereference of the `value` member of `ft`
+ 5) print out the results with some creative couts 
+    i.e.
+         FloatType ft(0.1f);
+         IntType it(3);
+         std::cout << "adding 3 and subtracting 'it' from 'ft' results in the following value: " << *ft.add(2.f).subtract( it ).value << std::endl;  //note the dereference of the `value` member of `ft`
  
  6) Don't let your heap-allocated owned type leak!
  
- 7) click the [run] button.  Clear up any errors or warnings as best you can.
- 
+ 7) replace your main() with the main() below.
+    It has some intentional mistakes that you need to fix to match the expected output
+
+ 8) click the [run] button.  Clear up any errors or warnings as best you can.
  */
 
 
@@ -471,24 +473,109 @@ IntType& IntType::divide(const IntType& rhs)
     *value /= *(rhs.value);
     return *this;
 }
-int main() {
 
-    FloatType fta(9.99);
-    FloatType ftb(10.5);
-    DoubleType dta(9.99);
-    DoubleType dtb(10.5);
-    IntType ita(10);
-    IntType itb(10);
-    
-    std::cout << *fta.value << ", add " << *ftb.value << ", multiplied by " << *dtb.value << ", divided by " << *itb.value << ", gives the result: " << *fta.add(ftb).multiply(dtb).divide(itb).value << "." << std::endl;
-    
-    std::cout << *dta.value << ", add " << *ftb.value << ", multiplied by " << *dtb.value << ", divided by " << *itb.value << ", gives the result: " << *dta.add(ftb).multiply(dtb).divide(itb).value << "." << std::endl;
+int main()
+{   
+    //testing instruction 0
+    HeapA heapA ; 
 
-    std::cout << *ita.value << ", add " << *ftb.value << ", multiplied by " << *dtb.value << ", divided by " << *itb.value << ", gives the result: " << *ita.add(ftb).multiply(dtb).divide(itb).value << "." << std::endl;
+    //assign heap primitives
+    FloatType ft ( 2.0f );
+    DoubleType dt ( 2 );
+    IntType it ( 2 ) ;
+
+    std::cout << "FloatType add result=" << ( ft.add( 2.0f ).value ) << std::endl;
+    std::cout << "FloatType subtract result=" << ( ft.subtract( 2.0f ).value ) << std::endl;
+    std::cout << "FloatType multiply result=" << ( ft.multiply( 2.0f ).value ) << std::endl;
+    std::cout << "FloatType divide result=" << ( ft.divide( 16.0f).value ) << std::endl << std::endl;
+
+    std::cout << "DoubleType add result=" << ( dt.add(2.0).value ) << std::endl;
+    std::cout << "DoubleType subtract result=" << (dt.subtract(2.0).value) << std::endl;
+    std::cout << "DoubleType multiply result=" << (dt.multiply(2.0).value) << std::endl;
+    std::cout << "DoubleType divide result=" << (dt.divide(5.f).value) << std::endl << std::endl;
+
+    std::cout << "IntType add result=" << ( it.add(2).value ) << std::endl;
+    std::cout << "IntType subtract result=" << ( it.subtract(2).value ) << std::endl;
+    std::cout << "IntType multiply result=" << ( it.multiply(2).value ) << std::endl;
+    std::cout << "IntType divide result=" << ( it.divide(3).value ) << std::endl << std::endl;
+    std::cout << "Chain calculation = " << ( (it.multiply(1000).divide(2).subtract(10).add(100)).value ) << std::endl;
+
+        // FloatType object instanciation and method tests
+    // --------
+    std::cout << "New value of ft = (ft + 3.0f) * 1.5f / 5.0f = " << (ft.add( 3.0f ).multiply(1.5f).divide(5.0f).value) << std::endl;
+       
+    std::cout << "---------------------\n" << std::endl; 
     
+    // DoubleType/IntType object instanciation and method tests
+    // --------
+    std::cout << "Initial value of dt: " << (dt.value) << std::endl;
+    std::cout << "Initial value of it: " << (it.value) << std::endl;
+    // --------
+    std::cout << "Use of function concatenation (mixed type arguments) " << std::endl;
+    std::cout << "New value of dt = (dt * it) / 5.0f + ft = " << (dt.multiply(it).divide(5.0f).add(ft).value) << std::endl;
+
+    std::cout << "---------------------\n" << std::endl; 
     
-    
+    // Intercept division by 0
+    // --------
+    std::cout << "Intercept division by 0 " << std::endl;
+    std::cout << "New value of it = it / 0 = " << (it.divide(0).value) << std::endl;
+    std::cout << "New value of ft = ft / 0 = " << (ft.divide(0).value) << std::endl;
+    std::cout << "New value of dt = dt / 0 = " << (dt.divide(0).value) << std::endl;
+
+    std::cout << "---------------------\n" << std::endl; 
+
+    std::cout << "good to go!\n";
+
     return 0;
 }
+
+/*
+your program should generate the following output.  
+Use a service like https://www.diffchecker.com/diff to compare your output. 
+you'll learn to solve the conversion warnings in the next project part.
+
+18 warnings generated.
+FloatType add result=4
+FloatType subtract result=2
+FloatType multiply result=4
+FloatType divide result=0.25
+
+DoubleType add result=4
+DoubleType subtract result=2
+DoubleType multiply result=4
+DoubleType divide result=0.8
+
+IntType add result=4
+IntType subtract result=2
+IntType multiply result=4
+IntType divide result=1
+
+Chain calculation = 590
+New value of ft = (ft + 3.0f) * 1.5f / 5.0f = 0.975
+---------------------
+
+Initial value of dt: 0.8
+Initial value of it: 590
+Use of function concatenation (mixed type arguments) 
+New value of dt = (dt * it) / 5.0f + ft = 95.375
+---------------------
+
+Intercept division by 0 
+New value of it = it / 0 = error: integer division by zero is an error and will crash the program!
+590
+New value of ft = ft / 0 = warning: floating point division by zero!
+inf
+New value of dt = dt / 0 = warning: floating point division by zero!
+inf
+---------------------
+
+good to go!
+
+
+
+
+
+*/
 
 
